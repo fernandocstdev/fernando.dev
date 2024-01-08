b8f5b020fd7228d273487f812b87a015a3c9c3a5
fernandocst03 <fernando.dorantescst@gmail.com>
2024-01-08 15:02:18 -0600

    Add adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import netlify from "@astrojs/netlify";
-
 
 // https://astro.build/config
 export default defineConfig({
