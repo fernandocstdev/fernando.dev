a8e1247478e431ab3c11e7f7407fa3682ddff065
fernandocst03 <fernando.dorantescst@gmail.com>
2024-01-11 17:10:36 -0600

    Delete projects todo App, todo list

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -2,13 +2,6 @@ import { type Project } from '../types/project'
 
 export const PROJECTS: Project[] = [
 	{
-		name: 'Notes App',
-		description: 'Aplicacion mobil de notas.',
-		technologies: ['Swift' , 'SwiftUI'],
-		repository: 'https://github.com/fernandocstdev/NotesApp',
-		date: new Date('12/05/2023'),
-	},
-	{
 		name: 'Control de gastos',
 		description: 'Este itio web permite al usuario tener una gestion y un control sobre los gastos que el usuario vaya realizando. Se añadieron graficas para indicarle al usuario cuanto porcentaje de su presupuesto habia gastado.',
 		technologies: ['JavaScript', 'CSS', 'React'],
@@ -25,15 +18,6 @@ export const PROJECTS: Project[] = [
 		date: new Date('11/17/2023'),
 	},
 	{
-		name: 'Space X Launch',
-		description: 'Sitio web con informacion acerca de los lanzamientos que ha realizado la compañia Space X, la informacion sobre los lanzamientos se consiguio consumiento la api de spacexdata.',
-		repository:'https://github.com/fernandocstdev/spacex-launch-app',
-		technologies: ['Astro', 'Tailwind', 'JavaScript'],
-		url: 'space-x-launch',
-		date: new Date('09/25/2023'),
-		image: 'https://res.cloudinary.com/doby4wtg5/image/upload/v1704746751/space-x_qxskxc.webp'
-	},
-	{
 		name:'Todo List',
 		description: 'Sitio web para crear lista de tareas, permitiendo al usuario marcar una tarea cuando ya este terminada y poder filtrar por tareas completadas y no completadas.',
 		repository: 'https://github.com/fernandocstdev/todo-list',
@@ -42,12 +26,4 @@ export const PROJECTS: Project[] = [
 		date: new Date('09/25/2023'),
 		image: 'https://res.cloudinary.com/doby4wtg5/image/upload/v1704746764/todo-list_uifdvj.webp'
 	},
-	{
-		name:'Spotify Clone',
-		description: 'Clon de Spotify creado con astro, en este proyecto utilizamos las nuevas transiciones que se añadieron en Astro 3, así como los diferntes estados de carga de un componente.',
-		repository: 'https://github.com/fernandocstdev/spotify-clone',
-		technologies: ['TypeScript', 'Next Js', 'Tailwind', 'React'],
-		date: new Date('01/04/2024'),
-		image: 'https://res.cloudinary.com/doby4wtg5/image/upload/v1704746759/spotify_b1vufm.webp'
-	}
 ]
